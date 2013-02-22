adc664687329719205a271f608e5b11f303ef98e
Fernando Hernandez <zetahernandez@gmail.com>
2013-02-22 16:17:43 -0300

    examples

---
+++ examples/web-example/public/config.js
@@ -0,0 +1,26 @@
+define({
+	app_name: "Example",
+	shim: {
+		'ember': {
+			deps: ['handlebars', 'jquery'],
+			exports: 'Ember'
+		},
+		'ember_data': {
+			deps: ['ember'],
+			exports: 'DS'
+		}
+	},
+	paths: {
+		// 'App': 'app/main',
+		// 'models': 'app/models',
+		// 'views': 'app/views',
+		// 'router': 'app/views',
+		// 'controllers': 'app/controllers',
+		// 'templates': 'app/templates',
+		/* libs */
+		'jquery': 'libs/jquery',
+		'handlebars': 'libs/handlebars',
+		'ember': 'libs/ember',
+		'ember_data': 'libs/ember-data'
+	}
+});
---
+++ examples/web-example/public/main.js
@@ -0,0 +1,9 @@
+(function (root) {
+	require(["config"], function(config){
+		requirejs.config(config);
+		require(["ember","ember_data"], function(Ember,DS){
+			
+		});
+	});
+	//this es el windows que pasa como parametro al root
+})(this);
