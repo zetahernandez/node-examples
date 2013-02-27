ca9cdc44fa33b1ede216dc2daff7a5d76beb2c86
Fernando Hernandez <zetahernandez@gmail.com>
2013-02-26 21:15:32 -0300

    change config

--- vagrant/my_config-template.rb
+++ vagrant/my_config-template.rb
@@ -1,1 +1,4 @@
-#$box = 'ubuntu'
+$box = 'debian_squeeze_32'
+$box_url = 'http://mathie-vagrant-boxes.s3.amazonaws.com/debian_squeeze_32.box'
+$repo = 'D:\Desarrollo\workspaces\book-identification\node-examples\examples'
+		 
